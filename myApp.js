ebab6b294fb39a4b96f4f302c0e24923aca40262
Brittney Epperson <beppers87@hotmail.com>
2024-07-31 20:43:05 +0000

    Added methods for find by food and find by id

--- myApp.js
+++ myApp.js
@@ -42,11 +42,17 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function(err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
